7611bbaa16b6a4d912aa2637b2e0f453573d084a
Abhi_Joshi <joshi.abhiram98@gmail.com>
2024-02-24 20:28:17 +0000

    working no move check

--- 8puzzle/Solver.java
+++ 8puzzle/Solver.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 
 import edu.princeton.cs.algs4.MinPQ;
@@ -9,21 +8,19 @@ public class Solver {
         int priority;
         int distance;
         int movesMade;
-        int[] move;
         Board board;
         Node prevBoard;
 
-        public Node (int prevMoves, Board nodeBoard, Node prevBoard, int[] moveToBoard, String distanceFunc){
+        public Node (int prevMoves, Board nodeBoard, Node prevBoard, String distanceFunc){
             this.board = nodeBoard;
             this.prevBoard = prevBoard;
             this.movesMade = prevMoves+1;
-            this.move = moveToBoard;
             this.distance = calcDistance(distanceFunc);
             this.priority = movesMade + distance;
         }
 
         private int calcDistance(String distanceFunc) {
-            if (this.move == null) {
+            if (this.prevBoard == null) {
                 if (distanceFunc == "hamming") {
                     return board.hamming();
                 } else {
@@ -37,48 +34,13 @@ public class Solver {
         }
 
         private int intermediateManhattan() {
-            int n = board.n;
-            int blankRow = board.posBlank/n;
-            int blankCol = board.posBlank%n;
-            int valueRow = blankRow - this.move[0];
-            int valueCol = blankCol - this.move[1];
-            int value = board.board[valueRow][valueCol];
-
-            int curr_distance = Math.abs(valueRow - ((value - 1) / n)) + Math.abs(valueCol - ((value - 1) % n));
-            int prev_distance = Math.abs(blankRow - ((value - 1) / n)) + Math.abs(blankCol - ((value - 1) % n));
-            
-            return prevBoard.distance + (curr_distance-prev_distance);
+            return board.manhattan();
         }
 
         private int intermediateHamming() {
-            int n = board.n;
-            int blankRow = board.posBlank/n;
-            int blankCol = board.posBlank%n;
-            int valueRow = blankRow + this.move[0];
-            int valueCol = blankRow + this.move[1];
-            int value = board.board[valueRow][valueCol];
-
-            int curr_distance = (valueRow*n + valueCol == value) ? 1 : 0;
-            int prev_distance = (blankRow*n + blankCol == value) ? 1 : 0;
-            
-            return prevBoard.distance + (curr_distance-prev_distance);
+            return board.hamming();
         }
 
-        public int getPriority() {
-            return this.priority;
-        }
-
-        public int getDistance() {
-            return this.distance;
-        }
-
-        public int getMoves() {
-            return this.movesMade;
-        }
-
-        public Board getBoard() {
-            return this.board;
-        }
     }
 
     private class NodeComparator implements Comparator<Node> {
@@ -108,8 +70,8 @@ public class Solver {
         movesToSolve = -1;
         Node solvedNode = null;
         Board initialTwin = initial.twin();
-        Node initialNode = new Node(-1, initial, null, null, this.distanceFunc);
-        Node initialNodeTwin = new Node(-1, initialTwin, null, null, this.distanceFunc);
+        Node initialNode = new Node(-1, initial, null, this.distanceFunc);
+        Node initialNodeTwin = new Node(-1, initialTwin, null, this.distanceFunc);
         gameTree = new MinPQ<>(new NodeComparator());
         gameTree.insert(initialNode); 
         gameTreeTwin = new MinPQ<>(new NodeComparator());
@@ -139,42 +101,22 @@ public class Solver {
     }
 
     private void addNeighbours(Node currNode, MinPQ<Node> tree) {
-        for (int[] m: new int[][]{{0,1}, {0,-1},{1,0}, {-1,0}}) {
-            if (Arrays.equals(negativeArray(currNode.move), m)) {
+        Iterable<Board> neighbourBoards = currNode.board.neighbors();
+        for (Board b: neighbourBoards) {
+            if (currNode.prevBoard == null) {
+                ;
+            }
+            else if (currNode.prevBoard.board.equals(b)) {
                 continue;
             }
-            Node addNode = getNeighbour(currNode, m);
-            if (addNode != null) {
-                tree.insert(addNode);
-            }
+            Node neighbourNode = new Node(
+                currNode.movesMade, 
+                b, 
+                currNode, 
+                this.distanceFunc
+            );
+            tree.insert(neighbourNode);
         }
-
-    }
-
-    private Node getNeighbour(Node currNode, int[] m) {
-        int newRow = currNode.board.posBlank / currNode.board.n + m[0];
-        int newCol = currNode.board.posBlank % currNode.board.n + m[1];
-
-        if (!currNode.board.isValidPosition(newRow, newCol)) {
-            return null;
-        }
-        int[][] newBoard = currNode.board.copyBoard(currNode.board.board);
-        currNode.board.swap(newBoard, currNode.board.posBlank / currNode.board.n, currNode.board.posBlank % currNode.board.n, newRow, newCol);
-        Node neighbour = new Node(currNode.movesMade, new Board(newBoard), currNode, m, this.distanceFunc);
-
-        return neighbour;
-    }
-
-    private int[] negativeArray(int[] array) {
-        if (array == null) {
-            return null;
-        }
-
-        int[] negArray = new int[array.length];
-        for (int i = 0; i<array.length; i++) {
-            negArray[i] = -1*array[i];
-        }
-        return negArray;
     }
 
     public int moves() {
